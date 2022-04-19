234bc4ff43ce442ac348b32406d1f493f50a65b7
Naufal Muhamamd Farras <nmfarras@gmail.com>
2022-04-20 04:54:13 +0700

    [FIX] env config to include mail host and port and refactor some name

--- src/MailSender.js
+++ src/MailSender.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 class MailSender {
   constructor() {
     this._transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 465,
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
       secure: true,
       auth: {
         user: process.env.MAIL_ADDRESS,
@@ -13,7 +13,7 @@ class MailSender {
     });
   }
  
-  sendEmail(targetEmail, playlist) {
+  sendEmail(targetEmail, content) {
     const message = {
       from: 'Openmusic Apps',
       to: targetEmail,
@@ -22,7 +22,7 @@ class MailSender {
       attachments: [
         {
           filename: 'playlists.json',
-          playlist,
+          content,
         },
       ],
     };
